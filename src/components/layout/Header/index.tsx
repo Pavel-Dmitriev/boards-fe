0820c0c654d9317468047b8875b852ee212ba7bc
pdmitriev <pdmitriev@fil-it.ru>
2026-06-08 13:14:17 +0300

    feat: при logout перебрасывать на страницу входа

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -19,8 +19,8 @@ export function Header() {
   const isDark = useIsDark();
   const navigate = useNavigate();
 
-  const handleLogout = () => {
-    logout();
+  const handleLogout = async () => {
+    await logout();
     navigate("/login");
   };
 
